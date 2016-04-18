b5151f0e1c908b3b40a88fa541ac7fba673177f3
rikey <zhongrui8@360buyAD.local>
2016-04-18 11:01:54 +0800

    用户注册

---
+++ src/main/java/org/rikey/web/dao/UserDao.java
@@ -0,0 +1,11 @@
+package org.rikey.web.dao;
+
+/**
+ * @Description:
+ * @Since 2016/4/18
+ * @Author zhongrui8
+ * @Changelist 1. 2016/4/18 create.
+ * @Notice 如有改动，请在changelist中注明
+ */
+public interface UserDao {
+}
