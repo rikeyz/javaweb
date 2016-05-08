672ea3126d698aab83898f2c567af6c7e07ac8da
rikey <zhongrui8@360buyAD.local>
2016-05-08 08:47:49 +0800

    增加验证码

---
+++ src/main/java/org/rikey/web/utils/RandomImageGenerator.java
@@ -0,0 +1,11 @@
+package org.rikey.web.utils;
+
+/**
+ * @Description:
+ * @Since 2016/5/7
+ * @Author zhongrui8
+ * @Changelist 1. 2016/5/7 create.
+ * @Notice 如有改动，请在changelist中注明
+ */
+public class RandomImageGenerator {
+}
